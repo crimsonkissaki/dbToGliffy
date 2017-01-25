1c2e0ade3c34ba03e6f8acc7a69ad562fac03d53
Evan Johnson <ejohnson@stoneeagle.com>
2017-01-25 17:57:30 -0600

    Fixed join_dicts to handle >2 dicts & recursively update sub-dicts with the values of latter dict sub-dicts.

--- utils/util.py
+++ utils/util.py
@@ -32,28 +32,56 @@ def merge_dicts(*dicts):
     return r
 
 
-def join_dicts(to_dict={}, from_dict={}):
-    # type: (dict, dict) -> None
+def join_dicts(*dicts):
+    # type: (*dict) -> dict
     """
-    Recursively updates ``to_dict`` with the values from matching keys in ``from_dict``, if any.
-    If both dicts have sub-dicts those dicts will also be joined.
+    Recursively updates the first dict 'in place' with values from subsequent dicts if they have the same key.
 
-    NOTE: This method will modify ``to_dict`` in place.
+    The value from the last dict the key is found in (if any) becomes the final value
+    of the key in the first dict.
 
-    :param dict to_dict: The dict whose values need to be updated
-    :param dict from_dict: The dict that has new values
-    :return: ``to_dict`` with values updated from ``from_dict``
+    :param tuple dicts: Dicts to merge
+    :return:
     :rtype: dict
     """
-    if not isinstance(to_dict, dict) or not isinstance(from_dict, dict):
-        raise TypeError('Method `join_dicts()` only works with dict-like objects.')
+    # the only values that matter in the end are the ones in the right-most dicts
+    # looping over them in 'right -> left' order saves time
+    # TODO: how MUCH time does it save???
+    dicts = list(dicts)
+    res = dicts.pop(0)
+    dicts.reverse()
 
-    if not from_dict:
-        return to_dict
+    '''
+    Using a function since there's no way to break out of nested loops, and
+      we don't want to keep looking if we've already found a value to assign.
+    We also have to raise an exception since there's no way to tell if the
+      found value is actually 'None' or if the function is simply returning
+      'None' by default because no value was found.
+    '''
+    def _find_key(_key, _dicts):
+        # type: (str, list) -> Any
+        for _d in _dicts:
+            if not isinstance(_d, dict):
+                # you could raise here, but what if the dict has a 'none' value for the key that needs to be moved?
+                continue
+            if _key in _d:
+                return _d[_key]
+        raise ValueError('no value')
 
-    for d in to_dict:
-        if d in from_dict:
-            if isinstance(to_dict[d], dict) and isinstance(from_dict[d], dict):
-                join_dicts(to_dict[d], from_dict[d])
+    for key in res:
+        try:
+            # if isinstance(res[key], dict) and isinstance(new_val, dict):
+            if isinstance(res[key], dict):
+                # need to grab all the sub-dicts from each of the remaining dicts
+                # and un-reverse them so they will be handled properly
+                dicts_key = [d[key] for d in dicts if d[key]]
+                dicts_key.reverse()
+                join_dicts(res[key], *dicts_key)
             else:
-                to_dict[d] = from_dict[d]
+                res[key] = _find_key(key, dicts)
+        except ValueError:
+            continue
+
+    return res
+
+
